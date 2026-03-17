47a8dcc9b2d754fad547f8372ca208ea8119dd8a
Gurikov Maxim <maximgurikoff@gmail.com>
2026-03-17 17:59:27 +0500

    fixes after review

--- unit-tests-example/UserManagerTests/UserManagerTests.cs
+++ unit-tests-example/UserManagerTests/UserManagerTests.cs
@@ -18,7 +18,7 @@ public class UserManagerTests
         _newInvalidUser = new User("Invalid name", "invalid password", "invalid email address", 421);
         _allSavedUsers = [_user1, _user2, _user3];
     }
-    
+
     [SetUp]
     public void SetUp()
     {
@@ -32,90 +32,91 @@ public class UserManagerTests
             _userRepository.GetUser(user.Email).Returns(user);
             _userValidator.Validate(user).Returns((true, ""));
         }
-        
+
         _userValidator.Validate(_newInvalidUser).Returns((false, "Invalid user"));
         _userValidator.Validate(_newValidUser).Returns((true, ""));
-        
+
         _userManager = new UserManager(_userRepository, _emailService, _userValidator);
     }
-    
+
     [Test]
     public void CreateUser_ShouldSaveUserAndSandEmail_IfNewUserIsValid()
     {
         _userManager.CreateNewUser(_newValidUser);
-        
-        Received.InOrder(() => {
+
+        Received.InOrder(() =>
+        {
             _userValidator.Validate(_newValidUser);
             _userRepository.GetUser(_newValidUser.Email);
             _userRepository.SaveUser(_newValidUser);
             _emailService.SendEmail(_newValidUser.Name, "Welcome", "Thank you for registering!");
         });
     }
-    
+
     [Test]
     public void CreateUser_ShouldThrowInvalidUserExceptionWithReason_IfNewUserValidationFailed()
     {
-        var exception = Assert.Throws<InvalidUserException>(() => _userManager.CreateNewUser(_newInvalidUser));
+        Assert.That(() => _userManager.CreateNewUser(_newInvalidUser),
+            Throws.InstanceOf<InvalidUserException>().With.Message.EqualTo("Invalid user"));
         
-        Assert.That(exception!.Message, Is.EqualTo("Invalid user"));
         _userValidator.Received(1).Validate(_newInvalidUser);
         _emailService.DidNotReceive().SendEmail(_newInvalidUser.Name, "Welcome", "Thank you for registering!");
         _userRepository.DidNotReceive().SaveUser(_newInvalidUser);
         _userRepository.DidNotReceive().GetUser(_newInvalidUser.Email);
     }
-    
+
     [Test]
     public void CreateUser_ShouldThrowInvalidUserExceptionWithReason_IfUserWithThatEmailAlreadyExists()
     {
-        var exception = Assert.Throws<InvalidUserException>(() => _userManager.CreateNewUser(_user1));
-        
-        Assert.That(exception!.Message, Is.EqualTo("UserAlreadyExists"));
-        
-        Received.InOrder(() => {
+        Assert.That(() => _userManager.CreateNewUser(_user1),
+            Throws.InstanceOf<InvalidUserException>().With.Message.EqualTo("UserAlreadyExists"));
+
+        Received.InOrder(() =>
+        {
             _userValidator.Validate(_user1);
             _userRepository.GetUser(_user1.Email);
         });
-        
+
         _emailService.DidNotReceive().SendEmail(_user1.Name, "Welcome", "Thank you for registering!");
         _userRepository.DidNotReceive().SaveUser(_user1);
     }
-    
+
     [Test]
     public void DeleteUser_ShouldThrowInvalidOperationException_IfUserDoesNotExists()
     {
-        var exception = Assert.Throws<InvalidOperationException>(() => _userManager.DeleteUser(_newInvalidUser.Email));
-        
-        Assert.That(exception!.Message, Is.EqualTo("User not found"));
-        
+        Assert.That(() => _userManager.DeleteUser(_newInvalidUser.Email),
+            Throws.InvalidOperationException.With.Message.EqualTo("User not found"));
+
         _userRepository.Received(1).GetUser(_newInvalidUser.Email);
         _emailService.DidNotReceive().SendEmail(_newInvalidUser.Name, "Goodbye", "Your account has been deleted.");
         _userRepository.DidNotReceive().DeleteUser(_newInvalidUser.Email);
     }
-    
+
     [Test]
     public void DeleteUser_ShouldDeleteUserAndSendEmail_IfUserExists()
     {
         _userManager.DeleteUser(_user1.Email);
-        
-        Received.InOrder(() => {
+
+        Received.InOrder(() =>
+        {
             _userRepository.GetUser(_user1.Email);
             _userRepository.DeleteUser(_user1.Email);
             _emailService.SendEmail(_user1.Name, "Goodbye", "Your account has been deleted.");
         });
     }
-    
+
     [Test]
     public void GetAdultUsers_ShouldReturnUsersWithAgeGreaterOrEqualToEighteen()
     {
         var result = _userManager.GetAdultUsers();
-        
+
         Assert.That(result, Is.Not.Null);
-        Assert.That(result, Is.EquivalentTo(new List<User> {_user2, _user3}));
-        Assert.That(result.Select(x => x.Age), Is.All.GreaterThanOrEqualTo(18) );
-        
+        Assert.That(result, Is.EquivalentTo(new List<User> { _user2, _user3 }));
+        Assert.That(result.Select(x => x.Age), Is.All.GreaterThanOrEqualTo(18));
+
         _userRepository.Received(1).GetAllUsers();
     }
-    
+
     private IUsersRepository _userRepository;
     private IEmailService _emailService;
     private IUserValidator _userValidator;
